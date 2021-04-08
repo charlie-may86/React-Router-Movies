f71211f72dbd0373f38c27481af3bdb1975aba94
Charles May <charlie@Charless-MacBook-Pro.local>
2021-04-07 20:23:12 -0500

    movies are all displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Route } from "react-router-dom";
 
 import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -37,7 +40,14 @@ export default function App() {
         }
       />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/">
+          <MovieList movies={movieList} />
+        </Route>
+        <Route exact path="/movies/:id">
+          <Movie />
+        </Route>
+      </div>
     </div>
   );
 }
